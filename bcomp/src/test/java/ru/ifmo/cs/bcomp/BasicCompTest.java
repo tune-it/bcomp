82bb8a32ea995bb6b5462179bd24e4f74c2b73b3
Dmitry Afanasiev <Dmitry.Afanasiev@Tune-IT.Ru>
2014-03-09 21:25:08 +0000

    BasicCompTest fully refactored by me

--- bcomp/src/test/java/ru/ifmo/cs/bcomp/BasicCompTest.java
+++ bcomp/src/test/java/ru/ifmo/cs/bcomp/BasicCompTest.java
@@ -1,48 +1,258 @@
 /*
  * $Id$
  */
-
 package ru.ifmo.cs.bcomp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.EnumMap;
+import java.util.HashMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import static java.lang.Integer.parseInt;
+import static ru.ifmo.cs.bcomp.Utils.isHexNumeric;
+import static ru.ifmo.cs.bcomp.Utils.toHex;
 
 /**
  *
+ * @author Dmitry Afanasiev <KOT@MATPOCKuH.Ru>
  * @author Anastasia Prasolova <a-prasolova1507@yandex.ru>
  */
 public class BasicCompTest {
+	private static final CPU.Reg[] REGS_TO_TEST = {
+		CPU.Reg.ACCUM, CPU.Reg.ADDR, CPU.Reg.DATA, CPU.Reg.IP, CPU.Reg.INSTR
+	}
 
-	static MicroPrograms mps;
-	static MicroProgram mprog;
-	BasicComp bcomp;
-	Assembler asm;
-	String testString;
-	Instruction[] instr;
-	TestsSet testsSet = new TestsSet();
+	private static final int[] FLAGS_TO_TEST = {
+		StateReg.FLAG_C, StateReg.FLAG_Z, StateReg.FLAG_N, StateReg.FLAG_EI
+	};
 
-	@org.junit.Test
-	public void main() throws Exception {
-		mps = new MicroPrograms();
+	private static final List<String> CONTROL_OPS =
+		Arrays.asList("ADDR", "WRITE", "READ", "START");
 
-		String[] mprograms = {
-			"base", "optimized", "extended"
-		};
-		CommandToTest cmd;
-		KeyOperationsToTest keyOp;
+	private static final String[] baseInstrSetTests = {
+		//		"CLA; cmds = INC; check = АCCUM = 0",
+		//		"CLC; data = 020 = 9000, 021 = 8000; cmds = CLA, ADD 020, ADD 021; flags = C = 0",
+		"CMA;mem:020=8000;run:ADD 020;chk:Акк=7FFF,N=0",
+		"ADDR 100;chk:СК=100",
+	//		"CMC; data = 020 = 9000, 021 = 8000; cmds = CLA, ADD 020, ADD 021; flags = C = 0",
+	//		"ROL; data = 020 = 0004; cmds = CLA, CLC, ADD 020; regs = ACCUM = 0008; flags = C = 0",
+	//		"ROL; data = 020 = 8000; cmds = CLA, CLC, ADD 020; regs = ACCUM = 0; flags = C = 1",
+	//		"ROR; data = 020 = 0001; cmds = CLA, CLC, ADD 020; regs = ACCUM = 0; flags = C = 1",
+	//		"ROR; data = 020 = 8000; cmds = CLA, CLC, ADD 020; regs = ACCUM = 4000; flags = C = 0",
+	//		"INC; cmds = CLA; regs = ACCUM = 1; flags = C = 0, Z = 0, N = 0",//FAIL
+	//		"DEC; cmds = CLA; regs = ACCUM = FFFF; flags = Z = 0, C = 0, N = 1",//FAIL
+	//		"HLT",
+	//		"NOP",
+	//		"ADD 020; data = 020 = 9234; cmds = CLA; regs = ACCUM = 9234; flags = C = 0, N = 1, Z = 0",
+	//		"ADD 020; data = 020 = 9000, 021 = 8000; cmds = CLA, ADD 021; regs = ACCUM = 8000; flags = C = 1, N = 0, Z = 1",
+	//		"MOV 020; cmds = CLA, INC; memory = 020 = 1",
+	//		"AND 020; data = 020 = ABCD, 021 = FFFF; cmds = CLA, ADD 021; regs = ACCUM = ABCD; flags = N = 1, Z = 0",
+	//		"ADC 020; data = 020 = 1234, 021 = 9000, 022 = 8000; cmds = CLA, ADD 021, ADD 022; regs = ACCUM = 1235; flags = C = 0, Z = 0, N = 0",
+	//		"SUB 020; data = 020 = 0088, 021 = 1488; cmds = CLA, ADD 021; regs = ACCUM = 1400; flags = C = 0, N = 0, Z = 0",
+	//		"SUB 020; data = 020 = FFFF, 021 = 1234; cmds = CLA, ADD 021; regs = ACCUM = 1235; flags = C = 1, Z = 0, N = 0",
+	//		"ISZ 00B; data = 00B = FFFE; regs = INSTR = 011, DATA = FFFF; memory = 00B = FFFF",
+	//		"ISZ 00B; data = 00B = 0002; regs = INSTR = 012, DATA = 0003; memory = 00B = 0003",
+	//		"BCS 00B; data = 020 = 9000, 021 = 8000; cmds = CLA, ADD 020, ADD 021; regs = INSTR = 00B; flags = C = 1",
+	//		"BPL 00B; cmds = CLA, INC; regs = INSTR = 00B; flags = N = 0",
+	//		"BPL 00B; cmds = CLA; regs = INSTR = 00B; flags = Z = 1",
+	//		"BMI 00B; cmds = CLA, DEC; regs = INSTR = 00B; flags = N = 1",
+	//		"BEQ 00B; cmds = CLA; regs = INSTR = 00B; flags = Z = 1",
+	//		"BR 00B; regs = INSTR = 00B",
+	//		"JSR 00B; regs = INSTR = 00C; memory = 00B = 010",
+	//		"EI; flags = EI = 1",
+	//		"DI; flags = EI = 0",
+	//		"TSF 1; cmds = CLF 1; regs = IP = 011; flags = READY = 0",//fail
+	//		"CLF 1; flags = READY = 0",//fail
+	//		"IN 1; data = 020 = 6789; cmds = CLA, MOV 020, OUT 1; regs = ACCUM = 6789; flags = READY = 0",//fail
+	//		"OUT 1; data = 020 = 7890; cmds = CLA, MOV 020; regs = IODATA = 7890, ACCUM = 7890; flags = READY = 0"//fail
+	};
 
-		for (String mp : mprograms) {
+	private static final String[] extendedInstrSetTests = { //		"CLA; cmds = INC; check = АCCUM = 0",
+	//		"CLC; data = 020 = 9000, 021 = 8000; cmds = CLA, ADD 020, ADD 021; flags = C = 0",
+	//		"CMA; data = 020 = 8000; cmds = CLA, CLC, ADD 020; regs = АCCUM = 7FFF; flags = N = 0, Z = 0", //FAIL
+	//		"CMC; data = 020 = 9000, 021 = 8000; cmds = CLA, ADD 020, ADD 021; flags = C = 0",
+	//		"ROL; data = 020 = 0004; cmds = CLA, CLC, ADD 020; regs = ACCUM = 0008; flags = C = 0",
+	//		"ROL; data = 020 = 8000; cmds = CLA, CLC, ADD 020; regs = ACCUM = 0; flags = C = 1",
+	//		"SHL; data = 020 = FFFF, 021 = 0001; cmds = CLA, ADD 020, ADD 021; regs = ACCUM = FFFE; flags = C = 0",
+	//		"ROR; data = 020 = 0001; cmds = CLA, CLC, ADD 020; regs = ACCUM = 0; flags = C = 1",
+	//		"ROR; data = 020 = 8000; cmds = CLA, CLC, ADD 020; regs = ACCUM = 4000; flags = C = 0",
+	//		"SHR; data = 020 = FFFF, 021 = 0001; cmds = CLA, ADD 020, ADD 021; regs = ACCUM = 7FFF; flags = C = 0",
+	//		"INC; cmds = CLA, CLC; regs = ACCUM = 1; flags = C = 0, Z = 0, N = 0",//FAIL
+	//		"DEC; cmds = CLA; regs = ACCUM = FFFF; flags = C = 0, Z = 0, N = 1",//fail
+	//		"HLT",
+	//		"NOP",
+	//		"ADD 020; data = 020 = 9234; cmds = CLA; regs = ACCUM = 9234; flags = C = 0, N = 1, Z = 0",
+	//		"ADD 020; data = 020 = 9000, 021 = 8000; cmds = CLA, ADD 021; regs = ACCUM = 8000; flags = C = 1, N = 0, Z = 1",
+	//		"MOV 020; cmds = CLA, INC; memory = 020 = 1",
+	//		"AND 020; data = 020 = ABCD, 021 = FFFF; cmds = CLA, ADD 021; regs = ACCUM = ABCD; flags = N = 1, Z = 0",
+	//		"ADC 020; data = 020 = 1234, 021 = 9000, 022 = 8000; cmds = CLA, ADD 021, ADD 022; regs = ACCUM = 1235; flags = C = 0, Z = 0, N = 0",
+	//		"SUB 020; data = 020 = 0088, 021 = 1488; cmds = CLA, ADD 021; regs = ACCUM = 1400; flags = C = 0, N = 0, Z = 0",
+	//		"SUB 020; data = 020 = FFFF, 021 = 1234; cmds = CLA, ADD 021; regs = ACCUM = 1235; flags = C = 1, z = 0, N = 0",
+	//		"ISZ 00B; data = 00B = FFFE; regs = INSTR = 011, DATA = FFFF; memory = 00B = FFFF",
+	//		"ISZ 00B; data = 00B = 0002; regs = INSTR = 012, DATA = 0003; memory = 00B = 0003",
+	//		"BCS 00B; data = 020 = 9000, 021 = 8000; cmds = CLA, ADD 020, ADD 021; regs = INSTR = 00B; flags = C = 1",
+	//		"BPL 00B; cmds = CLA, INC; regs = INSTR = 00B; flags = N = 0",
+	//		"BPL 00B; cmds = CLA; regs = INSTR = 00B; flags = Z = 1",
+	//		"BMI 00B; cmds = CLA, DEC; regs = INSTR = 00B; flags = N = 1",
+	//		"BEQ 00B; cmds = CLA; regs = INSTR  = 00B; flags = Z = 1",
+	//		"JUMP 00B; regs = INSTR = 00B",//unknown command
+	//		"SWAP; data = 7FE = 1234, 020 = 5678; cmds = CLA, ADD 020; regs = ACCUM = 1234; flags = C = 0, N = 0, Z = 0; memory = 7FE = 5678",
+	//		"PUSH; cmds = CLA, INC; regs = ACCUM = 1; memory = 7FE = 0001, 7FF = 7FE",
+	//		"PUSHF; cmds = CLA; flags = C = 0, Z = 1, N = 0; memory = 7FE = 0040, 7FF = 7FE",
+	//		"POP; data = 020 = 2345; cmds = CLA, ADD 020, MOV 7FE; regs = ACCUM = 2345; flags = C = 0, Z = 0, N = 0; memory = 7FF = 7FE",
+	//		"POPF; data = 7FE = 0020; flags = C = 0, N = 1, Z = 0",
+	//		"RET; cmds = CALL 00B; regs = INSTR = 011",
+	//		"CALL 00B; regs = INSTR = 00B; memory = 7FE = 011",
+	//		"EI; flags = EI = 1",
+	//		"DI; flags = EI = 0",
+	//		"CMP 021; data = 020 = 5678, 021 = FFFF; cmds = CLA, ADD 020; flags = C = 0, Z = 0, N = 1",
+	//		"CMP 021; data = 020 = 1234, 021 =  1234; cmds = CLA, ADD 020; flags = C = 0, Z = 1, N = 0",
+	//		"CMP 021; data = 020 = FFFF, 021 = FFFF; cmds = CLA, ADD 020; flags = C = 1, N = 0, Z = 0",
+	//		"LOOP 00B; data = 00B = A001; regs = INSTR = 011, DATA = A000; memory = 00B = A000",
+	//		"LOOP 00B; data = 00B = 8000; regs = INSTR = 012, DATA = 7FFF; memory = 00B = 7FFF",
+	//		"TSF 1; flags = READY = 1", //fail
+	//		"CLF 1; flags = READY = 0",//fail
+	//		"IN 1; data = 020 = 6789; cmds = CLA, MOV 020, OUT 1; regs = ACCUM = 6789; flags = READY = 0",//fail
+	//		"OUT 1; data = 020 = 7890; cmds = CLA, MOV 020; regs = IODATA = 7890, ACCUM = 7890; flags = READY = 0"//fail
+	};
 
-			mprog = mps.getMicroProgram(mp);
-			bcomp = new BasicComp(mprog);
-			instr = bcomp.getCPU().getInstructionSet();
-			asm = new Assembler(instr);
-			String[] tests = testsSet.getTestStrings(mp);
+	private static final HashMap<Instruction[], String[]> testsets
+		= new HashMap<Instruction[], String[]>();
 
-			for (String testString : tests) {
-				cmd = new CommandToTest(bcomp, asm, testString);
-				cmd.testingCommand();
+	static {
+		testsets.put(BaseInstrSet.instructions, baseInstrSetTests);
+		testsets.put(ExtendedInstrSet.instructions, extendedInstrSetTests);
+	}
+
+	@Test
+	public void testBasicComp() throws Exception {
+		for (String microprogram : MicroPrograms.getMicroProgramsList()) {
+			testMicroProgram(microprogram);
+		}
+	}
+
+	;
+
+	private void testMicroProgram(String microprogram) throws Exception {
+		MicroProgram mp = MicroPrograms.getMicroProgram(microprogram);
+		BasicComp bcomp = new BasicComp(mp);
+		Assembler asm = new Assembler(mp.instructionSet);
+		for (String test : testsets.get(bcomp.getCPU().getInstructionSet())) {
+			runTest(test, bcomp, asm);
+		}
+	}
+
+	private void prepareProgram(String cmds, CPU cpu, Assembler asm) throws Exception {
+		asm.compileProgram("ORG 10\nBEGIN:\n" + cmds + "\nHLT");
+		asm.loadProgram(cpu);
+	}
+
+	private void runTest(String test, BasicComp bcomp, Assembler asm) throws Exception {
+		CPU cpu = bcomp.getCPU();
+		String mpname = "Микропрограмма " + cpu.getMicroProgramName() + ": ";
+		String[] fields = test.split(";");
+		String cmd = fields[0];
+		String[] cmdfields = cmd.split(" ");
+		boolean isControlOp = CONTROL_OPS.contains(cmdfields[0]);
+		String run = null;
+		String[] data = null;
+		String[] checks = null;
+		EnumMap<CPU.Reg, Integer> regs = new EnumMap<CPU.Reg, Integer>(CPU.Reg.class);
+		HashMap<Integer, Integer> flags = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> mem = new HashMap<Integer, Integer>();
+
+		for (String field : Arrays.copyOfRange(fields, 1, fields.length)) {
+			String[] params = field.split(":");
+
+			if (params[0].equals("run")) {
+				run = params[1].replace(",", "\n");
+			} else if (params[0].equals("mem")) {
+				data = params[1].split(",");
+			} else if (params[0].equals("chk")) {
+				checks = params[1].split(",");
+			} else {
+				fail(mpname + "неизвестный параметр " + params[0]);
 			}
-			/*keyOp = new KeyOperationsToTest(bcomp.getCPU());
-			keyOp.testingKeyOperations();*/
 		}
 
+		if (data != null) {
+			for (String field : data) {
+				String[] pair = field.split("=");
+				cpu.runSetAddr(parseInt(pair[0], 16));
+				cpu.runWrite(parseInt(pair[1], 16));
+			}
+		}
+
+		cpu.runStart();
+
+		if (run != null) {
+			prepareProgram(run, cpu, asm);
+			cpu.setRunState(1);
+			cpu.start();
+			cpu.setRunState(0);
+		}
+
+		if (isControlOp) {
+			for (CPU.Reg reg : REGS_TO_TEST)
+				regs.put(reg, cpu.getRegValue(reg));
+		} else {
+			prepareProgram(cmd, cpu, asm);
+
+			regs.put(CPU.Reg.ACCUM, cpu.getRegValue(CPU.Reg.ACCUM));
+			int ip = cpu.getRegValue(CPU.Reg.IP);
+			int command = cpu.getMemoryValue(ip);
+			regs.put(CPU.Reg.ADDR, ip);
+			regs.put(CPU.Reg.DATA, command);
+			regs.put(CPU.Reg.IP, ip + 1);
+			regs.put(CPU.Reg.INSTR, command);
+		}
+
+		for (int flag : FLAGS_TO_TEST)
+			flags.put(flag, cpu.getStateValue(flag));
+
+		if (checks != null)
+			for (String chk : checks) {
+					String[] pair = chk.split("=");
+					int value = parseInt(pair[1], 16);
+					CPU.Reg reg;
+
+					if (isHexNumeric(pair[0]))
+						mem.put(parseInt(pair[0], 16), value);
+					else if ((reg = cpu.findRegister(pair[0])) != null)
+						regs.put(reg, value);
+					else
+						flags.put(StateReg.getFlag(pair[0]), value);
+			}
+
+		if (isControlOp) {
+			cpu.runSetAddr(0x10);
+
+			if (cmdfields[0].equals("ADDR"))
+				cpu.runSetAddr(parseInt(cmdfields[1], 16));
+			else if (cmdfields[0].equals("WRITE"))
+				cpu.runWrite(parseInt(cmdfields[1], 16));
+			else if (cmdfields[0].equals("READ"))
+				cpu.runRead();
+			else if (cmdfields[0].equals("START"))
+				cpu.runStart();
+		} else
+			cpu.start();
+
+		for (CPU.Reg reg : regs.keySet())
+			assertEquals(
+				mpname + cmd + ": " + cpu.getRegister(reg).fullname,
+				regs.get(reg), (Integer) cpu.getRegValue(reg));
+
+		for (int flag : flags.keySet())
+			assertEquals(
+				mpname + cmd + ": флаг " + StateReg.NAME[flag],
+				flags.get(flag), (Integer) cpu.getStateValue(flag));
+
+		for (int addr : mem.keySet())
+			assertEquals(
+				mpname + cmd + ": ОП(" + toHex(addr, 11) + ")",
+				mem.get(addr), (Integer) cpu.getMemoryValue(addr));
 	}
 }
