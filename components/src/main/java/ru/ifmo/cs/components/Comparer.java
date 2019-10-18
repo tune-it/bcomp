84c300d3980eca986ec1eca2882c91ae3f929b24
MATPOCKuH <KOT@MATPOCKuH.Ru>
2019-10-18 21:39:19 +0300

    Commit Comparer for history - not really used in CPU

--- components/src/main/java/ru/ifmo/cs/components/Comparer.java
+++ components/src/main/java/ru/ifmo/cs/components/Comparer.java
@@ -8,19 +8,19 @@ package ru.ifmo.cs.components;
  *
  * @author Dmitry Afanasiev <KOT@MATPOCKuH.Ru>
  */
-public class Comparer extends DataHandler {
+public class Comparer extends Control {
 	private final DataSource input;
-	private final int startbit;
+	private final long valuestartbit;
 
-	public Comparer(DataSource input, int startbit, DataSource ... ctrls) {
-		super(1, ctrls);
+	public Comparer(DataSource input, long startbit, DataDestination ... dsts) {
+		super(1, 0, 0, dsts);
 
 		this.input = input;
-		this.startbit = startbit;
+		this.valuestartbit = startbit;
 	}
 
 	@Override
-	public void setValue(int ctrl) {
-		super.setValue(((input.getValue() >> startbit) & 1) == ctrl ? 1 : 0);
+	public void setValue(long value) {
+		System.out.println("Input = " + Long.toHexString((input.getValue() >> valuestartbit) & 1) + " Value = " + Long.toHexString(value));
 	}
 }
