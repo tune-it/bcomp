d119390b3e92f35832fdcb9475fcbc90db07f2d5
MATPOCKuH <KOT@MATPOCKuH.Ru>
2019-10-23 23:25:39 +0300

    MicroCode: fix lost HTOH + LTOL for SUB and CMP commands

--- bcomp/src/main/java/ru/ifmo/cs/bcomp/MicroCode.java
+++ bcomp/src/main/java/ru/ifmo/cs/bcomp/MicroCode.java
@@ -134,9 +134,9 @@ public class MicroCode {
         new omc(            cs(RDAC, RDDR, PLS1, HTOH, LTOL, STNZ, SETV, SETC, WRAC)),  // AC + DR + 1 -> BR, C, N, Z, V
         new CMC(            cs(RDPS, LTOL), PS0.ordinal(), 0,               "INT"),     // GOTO INT
         new CMC("CMD011X",  cs(RDCR, HTOL), 4, 1,                           "CMP"),     // if CR(12) = 1 then GOTO CMP
-        new omc("SUB",      cs(RDAC, RDDR, COMR, PLS1, STNZ, SETV, SETC, WRAC)),        // AC + ~DR + 1 -> AC, N, Z, V, C
+        new omc("SUB",      cs(RDAC, RDDR, COMR, PLS1, HTOH, LTOL, STNZ, SETV, SETC, WRAC)),// AC + ~DR + 1 -> AC, N, Z, V, C
         new CMC(            cs(RDPS, LTOL), PS0.ordinal(), 0,               "INT"),     // GOTO INT
-        new omc("CMP",      cs(RDAC, RDDR, COMR, PLS1, STNZ, SETV, SETC)),              // AC + ~DR + 1 -> N, Z, V, C
+        new omc("CMP",      cs(RDAC, RDDR, COMR, PLS1, HTOH, LTOL, STNZ, SETV, SETC)),	// AC + ~DR + 1 -> N, Z, V, C
         new CMC(            cs(RDPS, LTOL), PS0.ordinal(), 0,               "INT"),     // GOTO INT
         // Warning - 11XX was already checked
         new CMC("CMD1XXX",  cs(RDCR, HTOL), 5, 1,                           "CMD101X"), // if CR(13) = 1 then GOTO CMD101X
