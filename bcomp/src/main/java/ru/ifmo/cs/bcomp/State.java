46af4b02490e9b4894afbbaa0569e13656a1c0aa
MATPOCKuH <KOT@MATPOCKuH.Ru>
2019-10-26 21:14:06 +0300

    State: add flag's numbers

--- bcomp/src/main/java/ru/ifmo/cs/bcomp/State.java
+++ bcomp/src/main/java/ru/ifmo/cs/bcomp/State.java
@@ -10,51 +10,58 @@ package ru.ifmo.cs.bcomp;
  */
 public enum State {
 	/**
+	 * Flag 0:
 	 * Carry flag
 	 */
 	C,
 	/**
+	 * Flag 1:
 	 * Overflow flag
 	 */
 	V,
 	/**
+	 * Flag 2:
 	 * Zero flag
 	 */
 	Z,
 	/**
+	 * Flag 3:
 	 * Negative flag
 	 */
 	N,
 	/**
+	 * Flag 4:
 	 * IO flag???
 	 */
 	F,
 	/**
+	 * Flag 5:
 	 * Always zero
 	 */
 	PS0,
 	/**
+	 * Flag 6:
 	 * Enable Interrupts
 	 */
 	EI,
 	/**
+	 * Flag 7:
 	 * Interrupt
 	 */
 	INTR,
 	/**
+	 * Flag 8:
 	 * IOREADY
 	 */
 	IOREADY,
 	/**
+	 * Flag 9:
 	 * Run state
 	 */
 	RUN,
 	/**
+	 * Flag 10:
 	 * Program is running
 	 */
 	PROG,
-	/**
-	 * Use not assigned bit as zero for micro commands
-	 */
-	LAST_BIT,
 }
