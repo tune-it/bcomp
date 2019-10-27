2273d574b92ae3ab50aa89abba40f6fe46b538d7
Liza Medvedeva <medvedeva_elizaveta2000@mail.ru>
2019-10-27 19:51:35 +0300

    RunningCycles are changes

--- bcomp-ui/src/main/java/ru/ifmo/cs/bcomp/ui/components/RunningCycleView.java
+++ bcomp-ui/src/main/java/ru/ifmo/cs/bcomp/ui/components/RunningCycleView.java
@@ -10,6 +10,7 @@ import ru.ifmo.cs.bcomp.RunningCycle;
 import ru.ifmo.cs.bcomp.State;
 
 import java.awt.*;
+import java.util.EnumMap;
 
 import static ru.ifmo.cs.bcomp.ui.components.DisplayStyles.*;
 
@@ -19,7 +20,7 @@ import static ru.ifmo.cs.bcomp.ui.components.DisplayStyles.*;
  */
 public class RunningCycleView extends BCompComponent {
 	private CPU cpu;
-	private static final String[] cycles = {
+	private static final String[] cycleslabels = {
 		"Instruction fetch",
 		"Address fetch",
 		"Operand fetch",
@@ -28,39 +29,50 @@ public class RunningCycleView extends BCompComponent {
 		"Remote operation",
 		"Program"
 	};
-	private final JLabel[] labels = new JLabel[cycles.length];
-	private RunningCycle lastcycle = null;
+	private EnumMap<RunningCycle, Integer> cycles= new EnumMap<RunningCycle, Integer>(RunningCycle.class);
+
+	private final JLabel[] labels = new JLabel[cycleslabels.length];
+	private RunningCycle lastcycle = RunningCycle.HALT;
 	private long lastprogram = 0;
 
 	public RunningCycleView(CPU cpu, int x, int y) {
-		super("Control Unit", cycles.length);
+		super("Control Unit", cycleslabels.length);
 
 		this.cpu = cpu;
 
 		setBounds(x, y, 7*MEM_WIDTH/4);
 
-		for (int i = 0; i < cycles.length; i++) {
-			labels[i] = addValueLabel(cycles[i]);
+		for (int i = 0; i < cycleslabels.length; i++) {
+			labels[i] = addValueLabel(cycleslabels[i]);
 			labels[i].setBounds(1, getValueY(i), width - 2, CELL_HEIGHT);
 		}
+		cycles.put(RunningCycle.INFETCH,0);
+		cycles.put(RunningCycle.ADFETCH,1);
+		cycles.put(RunningCycle.OPFETCH,2);
+		cycles.put(RunningCycle.EXEC,3);
+		cycles.put(RunningCycle.INT,4);
+		cycles.put(RunningCycle.START,5);
+		cycles.put(RunningCycle.READ,5);
+		cycles.put(RunningCycle.WRITE,5);
+		cycles.put(RunningCycle.SETIP,5);
 	}
 
 	public void update() {
-//		RunningCycle newcycle = cpu.getRunningCycle();
-//		long newprogram = cpu.getProgramState(State.PROG);
-//
-//		if (newcycle != lastcycle) {
-//			if (lastcycle != null)
-//				labels[lastcycle.ordinal()].setForeground(COLOR_TEXT);
-//			if (newcycle != null)
-//				labels[newcycle.ordinal()].setForeground(COLOR_ACTIVE);
-//			lastcycle = newcycle;
-//		}
-//
-//		if (newprogram != lastprogram) {
-//			labels[labels.length - 1].setForeground(newprogram == 0 ? COLOR_TEXT : COLOR_ACTIVE);
-//			lastprogram = newprogram;
-//		}
+		RunningCycle newcycle = cpu.getRunningCycle();
+		long newprogram = cpu.getProgramState(State.PROG);
+
+		if (newcycle != lastcycle) {
+			if (lastcycle != RunningCycle.HALT)
+				labels[cycles.get(lastcycle)].setForeground(COLOR_TEXT);
+			if (newcycle != RunningCycle.HALT)
+			labels[cycles.get(newcycle)].setForeground(COLOR_ACTIVE);
+			lastcycle = newcycle;
+		}
+
+		if (newprogram != lastprogram) {
+			labels[labels.length - 1].setForeground(newprogram == 0 ? COLOR_TEXT : COLOR_ACTIVE);
+			lastprogram = newprogram;
+		}
 	}
 
 	@Override
