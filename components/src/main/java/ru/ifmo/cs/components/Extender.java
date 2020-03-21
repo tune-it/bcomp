d4449cdc68e7415bbf27550d050df65a1245cef9
MATPOCKuH <KOT@MATPOCKuH.Ru>
2020-03-22 00:46:18 +0300

    Extender.java: rename startbit to inputstartbit (conflicts which Control.java)

--- components/src/main/java/ru/ifmo/cs/components/Extender.java
+++ components/src/main/java/ru/ifmo/cs/components/Extender.java
@@ -10,18 +10,18 @@ package ru.ifmo.cs.components;
  */
 public class Extender extends Control {
 	private final DataSource input;
-	private final long startbit;
+	private final long inputstartbit;
 
 	public Extender(DataSource input, long width, long startbit, long ctrlbit, DataDestination ... dsts) {
 		super(width, 0, ctrlbit, dsts);
 
 		this.input = input;
-		this.startbit = startbit;
+		this.inputstartbit = startbit;
 	}
 
 	@Override
 	public synchronized void setValue(long value) {
 		if (isOpen(value))
-			super.setValue(((input.getValue() >> startbit) & 1L) == 1L ? mask : 0);
+			super.setValue(((input.getValue() >> inputstartbit) & 1L) == 1L ? mask : 0);
 	}
 }
