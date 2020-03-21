a0f6335ef8270cda196d23ab14de6a6cd1d9f870
MATPOCKuH <KOT@MATPOCKuH.Ru>
2020-03-22 00:51:34 +0300

    Restore Decoder from old staff (required for IO)

---
+++ components/src/main/java/ru/ifmo/cs/components/Decoder.java
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ */
+
+package ru.ifmo.cs.components;
+
+/**
+ *
+ * @author Dmitry Afanasiev <KOT@MATPOCKuH.Ru>
+ */
+public class Decoder extends Control {
+	private final DataSource input;
+	private final long inputstartbit;
+	private final long vmask;
+
+	public Decoder(DataSource input, long startbit, long width, long ctrlbit, DataDestination ... dsts) {
+		super(width, 0, ctrlbit, dsts);
+
+		this.input = input;
+		this.inputstartbit = startbit;
+		this.vmask = calculateMask(width);
+	}
+
+	@Override
+	public void setValue(long value) {
+		if (isOpen(value))
+			super.setValue(1 << ((value >> inputstartbit) & vmask));
+	}
+}
