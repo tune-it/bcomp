54e9e7cb6f3c78c60a7323c599aa035130ac0a86
MATPOCKuH <KOT@MATPOCKuH.Ru>
2020-03-28 19:18:46 +0300

    InputBus: added startbit and inputs into constructor

--- components/src/main/java/ru/ifmo/cs/components/InputBus.java
+++ components/src/main/java/ru/ifmo/cs/components/InputBus.java
@@ -13,12 +13,20 @@ import java.util.Arrays;
  */
 public class InputBus extends BasicComponent implements DataSource {
 	private final ArrayList<DataSource> inputs = new ArrayList<DataSource>();
+	private final long startbit;
 
-	public InputBus(long width) {
+	public InputBus(long width, long startbit, DataSource ... inputs) {
 		super(width);
+
+		this.startbit = startbit;
+		addInput(inputs);
 	}
 
-	public synchronized void addInput(DataSource ... inputs) {
+	public InputBus(long width, DataSource ... inputs) {
+		this(width, 0, inputs);
+	}
+
+	public final synchronized void addInput(DataSource ... inputs) {
 		this.inputs.addAll(Arrays.asList(inputs));
 	}
 
@@ -27,7 +35,7 @@ public class InputBus extends BasicComponent implements DataSource {
 		long value = 0;
 
 		for (DataSource input : inputs)
-			value |= input.getValue();
+			value |= input.getValue() >> startbit;
 
 		return value & mask;
 	}
